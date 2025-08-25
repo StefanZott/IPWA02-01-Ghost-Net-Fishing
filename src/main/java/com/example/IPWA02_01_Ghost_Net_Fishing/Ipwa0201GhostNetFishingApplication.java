1333c9ebe66a8f4e580bb1c0f96e5748ae378252
Stefan Zott <stefan@familiezott.de>
2025-08-25 22:16:15 +0200

    Add local .gitignore

---
+++ src/main/java/com/example/IPWA02_01_Ghost_Net_Fishing/Ipwa0201GhostNetFishingApplication.java
@@ -0,0 +1,13 @@
+package com.example.IPWA02_01_Ghost_Net_Fishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipwa0201GhostNetFishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipwa0201GhostNetFishingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IPWA02_01_Ghost_Net_Fishing/Ipwa0201GhostNetFishingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IPWA02_01_Ghost_Net_Fishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ipwa0201GhostNetFishingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
